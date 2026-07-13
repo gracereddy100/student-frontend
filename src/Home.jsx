a6b381d6014410915ed17f1c6d2f64e16eaba8a4
gracereddy100 <gracereddy100@gmail.com>
2026-07-13 15:47:42 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="home-box">
+      <h1 className="home-title"><i>Welcome to student club membership</i></h1>
+      <p className="home-text">Use the navigation bar above to give club details</p>
+    </div>
+  );
+}
+
+export default Home;
+import "./Home.css";
